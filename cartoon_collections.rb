b62f67e9cd66f6fef5b183d5678584c77f0bb021
reflective-refactoring-7309 <bristuttii@gmail.com>
2020-06-10 00:54:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,45 +1,5 @@
 def roll_call_dwarves(array)
   i = 0 
- while i < array.length 
-    puts "#{i + 1}. #{array[i]}"
-    i += 1 
-  end 
-end
-
-roll_call_dwarves(dwarves)
-
-
-
-
-def summon_captain_planet(array)
-  nu_array = []
-  i = 0 
-  while i < array.length 
-    nu_array << array[i].capitalize + "!"
-    i += 1 
-  end 
-  nu_array
-end
-
-summon_captain_planet(planeteer_calls)
-
-
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
-
-
-
-
-def roll_call_dwarves(array)
-  i = 0 
   while i < array.length 
     puts "#{i + 1}. #{array[i]}"
     i += 1 
