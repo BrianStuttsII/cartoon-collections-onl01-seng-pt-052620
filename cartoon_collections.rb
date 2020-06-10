fedb06ee0e529851a1f089097e3abc22df7294ee
reflective-refactoring-7309 <bristuttii@gmail.com>
2020-06-10 00:48:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
 def roll_call_dwarves# code an argument here
   # Your code here
+  
 end
 
 
@@ -8,9 +9,11 @@ def summon_captain_planet# code an argument here
   # Your code here
 end
 
+
 def long_planeteer_calls# code an argument here
   # Your code here
 end
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
